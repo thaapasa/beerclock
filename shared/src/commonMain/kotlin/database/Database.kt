bea8da8119eb6a7caec4d92e91bfd178e5040a18
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2023-10-11 09:24:02 +0300

    Revert "Remove Database initialization"
    This reverts commit 31294638e11878395d326995144dfada4b07b1c3.

--- shared/src/commonMain/kotlin/database/Database.kt
+++ shared/src/commonMain/kotlin/database/Database.kt
@@ -1,3 +1,16 @@
 package fi.tuska.beerclock.common.database
 
 import app.cash.sqldelight.db.SqlDriver
+
+expect object DriverFactory {
+    fun createDriver(): SqlDriver
+}
+
+fun createDatabase(): BeerDatabase {
+    val driver = DriverFactory.createDriver()
+    val database = BeerDatabase(driver)
+
+    // Do more work with the database (see below).
+
+    return database
+}
