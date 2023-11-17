f969bc7d6bf54793651e5278a51267ca6c702131
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2023-11-17 22:06:51 +0200

    Merge project setup to new build files

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,26 +5,16 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
-}
-
-/*
-plugins {
-    // this is necessary to avoid the plugins to be loaded multiple times
-    // in each subproject's classloader
-    kotlin("multiplatform").apply(false)
-    id("com.android.application").apply(false)
-    id("com.android.library").apply(false)
-    id("org.jetbrains.compose").apply(false)
-    id("app.cash.sqldelight").apply(false)
+    alias(libs.plugins.sqlDelight) apply false
 }
 
 buildscript {
     repositories {
         gradlePluginPortal()
+        mavenCentral()
     }
 
     dependencies {
-        classpath("dev.icerock.moko:resources-generator:0.23.0")
+        classpath(libs.mokoResourcesGenerator)
     }
 }
-*/
--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,10 +1,11 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.compose.ExperimentalComposeLibrary
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.sqlDelight)
+    id("dev.icerock.mobile.multiplatform-resources")
 }
 
 kotlin {
@@ -15,7 +16,7 @@ kotlin {
             }
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -26,13 +27,20 @@ kotlin {
             isStatic = true
         }
     }
-    
+
     sourceSets {
-        
+
         androidMain.dependencies {
             implementation(libs.compose.ui)
             implementation(libs.compose.ui.tooling.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.startupRuntime)
+            implementation(libs.sqlDelightAndroid)
+            /*
+                dependsOn(commonMain)
+                api("androidx.appcompat:appcompat:1.6.1")
+                api("androidx.core:core-ktx:1.12.0")
+             */
         }
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -40,16 +48,34 @@ kotlin {
             implementation(compose.material)
             @OptIn(ExperimentalComposeLibrary::class)
             implementation(compose.components.resources)
+            implementation(libs.voyager)
+            implementation(libs.mokoResourcesCompose)
         }
+
+        /*
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain.get())
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation(libs.sqlDelightNative)
+            }
+        }
+        */
     }
 }
 
 android {
-    namespace = "fi.tuska.beerclock"
+    namespace = "fi.tuska.beerclock.common"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     sourceSets["main"].res.srcDirs("src/androidMain/res")
+    // This causes a "duplicate content roots detected" error but it is actually needed.
     sourceSets["main"].resources.srcDirs("src/commonMain/resources")
 
     defaultConfig {
@@ -85,74 +111,6 @@ android {
 }
 
 
-/* 
-
-
-
-plugins {
-    kotlin("multiplatform")
-    id("com.android.library")
-    id("app.cash.sqldelight")
-    id("org.jetbrains.compose")
-    id("dev.icerock.mobile.multiplatform-resources")
-}
-
-kotlin {
-    androidTarget()
-
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "shared"
-            isStatic = true
-        }
-    }
-
-    val voyagerVersion = "1.0.0-rc07"
-
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.material)
-                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
-                implementation(compose.components.resources)
-                // Voyager is used to provide navigation for the app
-                implementation("cafe.adriel.voyager:voyager-navigator:$voyagerVersion")
-                implementation("dev.icerock.moko:resources-compose:0.23.0")
-            }
-        }
-        val androidMain by getting {
-            dependencies {
-                dependsOn(commonMain)
-                api("androidx.activity:activity-compose:1.8.1")
-                api("androidx.appcompat:appcompat:1.6.1")
-                api("androidx.core:core-ktx:1.12.0")
-                // The startup library is used for Android to provide a global reference to the
-                // Application Context
-                implementation("androidx.startup:startup-runtime:1.1.1")
-                implementation("app.cash.sqldelight:android-driver:2.0.0")
-            }
-        }
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-            dependencies {
-                implementation("app.cash.sqldelight:native-driver:2.0.0")
-            }
-        }
-    }
-}
-
 sqldelight {
     databases {
         create("BeerDatabase") {
@@ -169,30 +127,7 @@ multiplatformResources {
 }
 
 dependencies {
-    commonMainApi("dev.icerock.moko:resources:0.23.0")
-    commonMainApi("dev.icerock.moko:resources-compose:0.23.0") // for compose multiplatform
-    // commonTestImplementation("dev.icerock.moko:resources-test:0.23.0") // for testing
+    commonMainApi(libs.mokoResources)
+    commonMainApi(libs.mokoResourcesCompose) // for compose multiplatform
+    // commonTestImplementation(libs.mokoResourcesTest) // for testing
 }
-
-android {
-    compileSdk = (findProperty("android.compileSdk") as String).toInt()
-    namespace = "fi.tuska.beerclock.common"
-
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    sourceSets["main"].res.srcDirs("src/androidMain/res")
-    // This causes a "duplicate content roots detected" error but it is actually needed.
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-
-    defaultConfig {
-        minSdk = (findProperty("android.minSdk") as String).toInt()
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlin {
-        jvmToolchain(17)
-    }
-}
-
-*/
