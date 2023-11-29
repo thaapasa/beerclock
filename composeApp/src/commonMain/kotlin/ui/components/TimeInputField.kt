b521a777668a6971d1c91f266cad7b63f51eb819
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2023-11-29 10:20:54 +0200

    Finalize time picker dialog

--- composeApp/src/commonMain/kotlin/ui/components/TimeInputField.kt
+++ composeApp/src/commonMain/kotlin/ui/components/TimeInputField.kt
@@ -1,19 +1,34 @@
 package fi.tuska.beerclock.ui.components
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerLayoutType
+import androidx.compose.material3.TimePickerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.unit.dp
 import fi.tuska.beerclock.images.AppIcon
 import fi.tuska.beerclock.localization.strings
 import kotlinx.datetime.LocalTime
 
-fun LocalTime.toDisplayString() = strings.time(this)
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TimeInputField(
     value: LocalTime,
@@ -21,18 +36,48 @@ fun TimeInputField(
     modifier: Modifier = Modifier,
     labelText: String? = null
 ) {
+    var pickerShown by remember { mutableStateOf(false) }
+    var state = rememberTimePickerState(value.hour, value.minute, true)
+    LaunchedEffect(value) {
+        state = TimePickerState(value.hour, value.minute, true)
+    }
+
     OutlinedTextField(
-        value = value.toDisplayString(),
+        value = strings.time(value),
         onValueChange = {},
-        modifier = modifier,
+        modifier = modifier.onFocusChanged {
+            if (it.hasFocus) {
+                pickerShown = true
+            }
+        },
         label = { labelText?.let { Text(it) } },
         readOnly = true,
         leadingIcon = {
-            Icon(
-                painter = AppIcon.CLOCK.painter(),
-                contentDescription = labelText ?: strings.pickTime,
-                modifier = Modifier.size(16.dp)
-            )
+            IconButton({ pickerShown = true }) {
+                Icon(
+                    painter = AppIcon.CLOCK.painter(),
+                    contentDescription = labelText ?: strings.pickTime,
+                    modifier = Modifier.size(16.dp)
+                )
+            }
         }
     )
+    if (pickerShown) {
+        DatePickerDialog(
+            onDismissRequest = { pickerShown = false },
+            confirmButton = {
+                TextButton({
+                    onValueChange(LocalTime(state.hour, state.minute, 0))
+                    pickerShown = false
+                }) { Text(strings.pickTime) }
+            },
+        )
+        {
+            TimePicker(
+                state = state,
+                modifier = Modifier.align(Alignment.CenterHorizontally).padding(16.dp),
+                layoutType = TimePickerLayoutType.Vertical
+            )
+        }
+    }
 }
