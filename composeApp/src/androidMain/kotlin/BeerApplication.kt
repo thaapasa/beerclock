afa17d65c84328949c10c993d431072c324a314e
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2023-11-30 21:49:18 +0200

    Attach Android logger to Koin

--- composeApp/src/androidMain/kotlin/BeerApplication.kt
+++ composeApp/src/androidMain/kotlin/BeerApplication.kt
@@ -3,11 +3,14 @@ package fi.tuska.beerclock
 import android.app.Application
 import fi.tuska.beerclock.di.initKoin
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.logger.Level
 
 class BeerApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         initKoin {
+            androidLogger(level = Level.INFO)
             androidContext(applicationContext)
         }
     }
