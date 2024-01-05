b1d2a6009a5bc1e008dacdb58a6bc14094c6cb4a
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2024-01-05 17:37:43 +0200

    Add more drinks images

--- composeApp/src/commonMain/kotlin/drinks/ExampleDrinks.kt
+++ composeApp/src/commonMain/kotlin/drinks/ExampleDrinks.kt
@@ -23,13 +23,19 @@ class DrinkDef(
 fun createExampleDrinks(l: (en: String, fi: String) -> String) = listOf(
     DrinkDef(Category.BEERS, l("Large Lager", "Iso lager"), 50.0, 4.6, DrinkImage.BEER_GLASS_1),
     DrinkDef(Category.BEERS, l("Small Stout", "Pieni Stout"), 33.0, 4.6, DrinkImage.BEER_CAN_1),
-    DrinkDef(Category.CIDERS, l("Apple Cider", "Omenasiideri"), 33.0, 4.6, DrinkImage.BEER_CAN_1),
+    DrinkDef(
+        Category.CIDERS,
+        l("Apple Cider", "Omenasiideri"),
+        33.0,
+        4.6,
+        DrinkImage.CIDER_GLASS_1
+    ),
     DrinkDef(
         Category.CIDERS,
         l("Honey Mead", "Hunajaviini (sima)"),
         20.0,
         10.0,
-        DrinkImage.BEER_CAN_1
+        DrinkImage.CIDER_BOTTLE_1
     ),
     DrinkDef(Category.WINES, l("Red Wine", "Punaviini"), 16.0, 13.0, DrinkImage.RED_WINE_GLASS_1),
     DrinkDef(
@@ -44,16 +50,16 @@ fun createExampleDrinks(l: (en: String, fi: String) -> String) = listOf(
         l("Champagne", "Shamppanja"),
         12.0,
         12.0,
-        DrinkImage.WHITE_WINE_GLASS_1
+        DrinkImage.CHAMPAGNE_GLASS_1
     ),
-    DrinkDef(Category.COCKTAILS, "Margarita", 10.0, 20.0, DrinkImage.MARTINI_1),
+    DrinkDef(Category.COCKTAILS, "Margarita", 10.0, 20.0, DrinkImage.MARGARITA_1),
     DrinkDef(Category.COCKTAILS, "Martini", 10.0, 18.0, DrinkImage.MARTINI_1),
     DrinkDef(Category.SPIRITS, l("Cognac", "Konjakki"), 4.0, 43.0, DrinkImage.COGNAC_1),
     DrinkDef(Category.SPIRITS, l("Whisky", "Viski"), 4.0, 43.0, DrinkImage.COGNAC_1),
-    DrinkDef(Category.SPIRITS, "Vodka", 4.0, 40.0, DrinkImage.COGNAC_1),
-    DrinkDef(Category.WARM_DRINKS, "Irish Coffee", 20.0, 12.0, DrinkImage.COGNAC_1),
-    DrinkDef(Category.WARM_DRINKS, l("Mulled Wine", "Glögi"), 20.0, 10.0, DrinkImage.COGNAC_1),
-    DrinkDef(Category.SPECIALITY, "Sake", 10.0, 17.0, DrinkImage.COGNAC_1),
+    DrinkDef(Category.SPIRITS, "Vodka", 4.0, 40.0, DrinkImage.VODKA_1),
+    DrinkDef(Category.WARM_DRINKS, "Irish Coffee", 20.0, 12.0, DrinkImage.IRISH_COFFEE_1),
+    DrinkDef(Category.WARM_DRINKS, l("Mulled Wine", "Glögi"), 20.0, 10.0, DrinkImage.MULLED_WINE_1),
+    DrinkDef(Category.SPECIALITY, "Sake", 10.0, 17.0, DrinkImage.SAKE_GLASS_1),
     DrinkDef(Category.LOW_ALCOHOL, "Shandy", 50.0, 2.5, DrinkImage.COGNAC_1),
     DrinkDef(Category.LOW_ALCOHOL, "Spritzer", 20.0, 6.5, DrinkImage.COGNAC_1),
 )
