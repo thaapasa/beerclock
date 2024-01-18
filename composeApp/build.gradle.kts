bf187c9f8f816e53ec4e9bfa39512551a2822145
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2024-01-18 18:08:22 +0200

    Update version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -77,8 +77,8 @@ android {
         applicationId = "fi.tuska.beerclock"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
     }
     buildFeatures {
         compose = true
