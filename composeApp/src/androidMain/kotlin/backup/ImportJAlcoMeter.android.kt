2aad7ab64e3098566cd63d69a0e0de55ab338bdd
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2024-01-18 21:01:17 +0200

    Run data import in IO dispatcher

--- composeApp/src/androidMain/kotlin/backup/ImportJAlcoMeter.android.kt
+++ composeApp/src/androidMain/kotlin/backup/ImportJAlcoMeter.android.kt
@@ -5,11 +5,19 @@ import android.net.Uri
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import fi.tuska.beerclock.backup.jalcometer.importJAlcometerData
 import fi.tuska.beerclock.logging.getLogger
+import fi.tuska.beerclock.ui.composables.ViewModel
+import fi.tuska.beerclock.ui.composables.rememberWithDispose
 import io.requery.android.database.sqlite.SQLiteDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.koin.core.component.KoinComponent
 import java.io.File
 
 
@@ -20,36 +28,56 @@ actual fun isJAlcoMeterImportSupported(): Boolean {
 }
 
 @Composable
-actual fun ImportJAlkaMetriDataButton(title: String, modifier: Modifier) {
+actual fun ImportJAlkaMetriDataButton(
+    title: String,
+    modifier: Modifier,
+) {
     val context = LocalContext.current
+    val vm = rememberWithDispose { ImportJAlkaMetriViewModel(context) }
     FilePicker(
-        onFilePicked = { file ->
-            file?.let { importJAlcoMeterBackupData(context, it) }
-        },
-    ) { onClick -> Button(onClick = onClick) { Text(title) } }
+        onFilePicked = { file -> file?.let(vm::import) },
+    ) { onClick -> Button(onClick = onClick, enabled = !vm.importing) { Text(title) } }
 }
 
-fun importJAlcoMeterBackupData(context: Context, file: Uri) {
-    // We need to import the file in the app's own directory so that we can read it with SQLite
-    val importedFile = File(context.filesDir, "jalcometer-import.db")
-    if (!file.copyTo(context, importedFile)) {
-        return
-    }
-    try {
-        importJAlcoMeterDB(context, importedFile.path)
-    } finally {
-        when (importedFile.delete()) {
-            true -> logger.debug("Deleted temporary import file $importedFile")
-            false -> logger.warn("Could not delete temporary import file $importedFile")
+class ImportJAlkaMetriViewModel(private val context: Context) : ViewModel(), KoinComponent {
+    var importing by mutableStateOf(false)
+        private set
+    
+    fun import(file: Uri) {
+        if (importing) return
+        importing = true
+        launch(Dispatchers.IO) {
+            try {
+                importJAlcoMeterBackupData(context, file)
+            } finally {
+                importing = false
+            }
         }
     }
-}
 
-fun importJAlcoMeterDB(context: Context, filePath: String) {
-    logger.info("Loading imported jAlcoMeter DB from $filePath")
-    SQLiteDatabase.openDatabase(
-        filePath,
-        null,
-        SQLiteDatabase.OPEN_READONLY
-    ).use(::importJAlcometerData)
+    private fun importJAlcoMeterBackupData(context: Context, file: Uri) {
+        // We need to import the file in the app's own directory so that we can read it with SQLite
+        val importedFile = File(context.filesDir, "jalcometer-import.db")
+        if (!file.copyTo(context, importedFile)) {
+            return
+        }
+        try {
+            importJAlcoMeterDB(context, importedFile.path)
+        } finally {
+            when (importedFile.delete()) {
+                true -> logger.debug("Deleted temporary import file $importedFile")
+                false -> logger.warn("Could not delete temporary import file $importedFile")
+            }
+        }
+    }
+
+    private fun importJAlcoMeterDB(context: Context, filePath: String) {
+        logger.info("Loading imported jAlcoMeter DB from $filePath")
+        SQLiteDatabase.openDatabase(
+            filePath,
+            null,
+            SQLiteDatabase.OPEN_READONLY
+        ).use(::importJAlcometerData)
+    }
+
 }
