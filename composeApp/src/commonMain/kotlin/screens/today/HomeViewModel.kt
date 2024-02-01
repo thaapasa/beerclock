b514fde41470ee7dba7d4cd5e09f6403d4938ab4
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2024-02-01 17:28:07 +0200

    Make gauges use the max values from user preferences

--- composeApp/src/commonMain/kotlin/screens/today/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/screens/today/HomeViewModel.kt
@@ -17,13 +17,12 @@ import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
+import kotlin.math.max
 import kotlin.math.min
 import kotlin.time.Duration.Companion.seconds
 
 private val logger = getLogger("HomeViewModel")
 
-const val maxUnitsPosition = 7.0
-const val maxBacPosition = 1.0
 val pauseBetweenUpdates = 30.seconds
 
 class HomeViewModel : ViewModel(), KoinComponent {
@@ -67,13 +66,18 @@ class HomeViewModel : ViewModel(), KoinComponent {
         }
     }
 
+
+    fun maxBACValue(): Double = max(prefs.prefs.maxBAC, 0.1)
+    fun maxDailyUnitsValue(): Double = max(prefs.prefs.maxDailyUnits, 0.1)
+
+
     private fun setDrinks(newDrinks: List<DrinkRecordInfo>) {
         drinks.clear()
         val dayStart = times.dayStartTime(drinkDay)
         drinks.addAll(newDrinks.filter { it.time >= dayStart })
         bacStatus = BacStatus(newDrinks, drinkDay)
         units = drinks.sumOf { it.units() }.toFloat()
-        unitsPosition = min(units / maxUnitsPosition, 1.0).toFloat()
+        unitsPosition = min(units / maxDailyUnitsValue(), 1.0).toFloat()
         updateBacStatus()
     }
 
@@ -84,7 +88,7 @@ class HomeViewModel : ViewModel(), KoinComponent {
             bacStatus.atTime(now).alcoholGrams,
             prefs.prefs
         ).toFloat()
-        bacPosition = min(bac / maxBacPosition, 1.0).toFloat()
+        bacPosition = min(bac / maxBACValue(), 1.0).toFloat()
 
         if (drinkDay != times.currentDrinkDay()) {
             logger.info("New drink day starts, reload drinks")
