a66f2f69be9b18bc71312e1f54a3ef0e06762da9
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2024-03-15 22:30:53 +0200

    Prepare next version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -77,8 +77,8 @@ android {
         applicationId = "fi.tuska.beerclock"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 12
-        versionName = "0.0.12"
+        versionCode = 13
+        versionName = "0.0.13"
     }
     buildFeatures {
         compose = true
